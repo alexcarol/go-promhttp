5b8a9885be6ad7b921c0a1e905459125f44dda91
Alex Carol <acarolcampre@adobe.com>
2022-07-07 12:13:42 +0200

    Add size instrumentation for client request and response size

--- client.go
+++ client.go
@@ -1,10 +1,11 @@
 package promhttp
 
 import (
-	"net/http"
-
+	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	pph "github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"strings"
 )
 
 // Client embeds original http Client.
@@ -31,6 +32,29 @@ func instrumentClientWithConstLabels(namespace string, c *http.Client, reg prome
 				Subsystem:   subsystemHTTPOutgoing,
 				Name:        "requests_total",
 				Help:        "A counter for outgoing requests from the wrapped client.",
+				ConstLabels: constLabels,
+			},
+			[]string{"code", "method"},
+		),
+		requestSize: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Namespace:   namespace,
+				Subsystem:   subsystemHTTPOutgoing,
+				Name:        "request_size_histogram_bytes",
+				Help:        "Request size in bytes.",
+				Buckets:     []float64{100, 1000, 2000, 5000, 10000},
+				ConstLabels: constLabels,
+			},
+			[]string{"code", "method"},
+		),
+		responseContentLength: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+
+				Namespace:   namespace,
+				Subsystem:   subsystemHTTPOutgoing,
+				Name:        "response_content_length_histogram",
+				Help:        "Response content length in bytes.",
+				Buckets:     []float64{100, 1000, 2000, 5000, 10000},
 				ConstLabels: constLabels,
 			},
 			[]string{"code", "method"},
@@ -103,19 +127,77 @@ func instrumentClientWithConstLabels(namespace string, c *http.Client, reg prome
 		Transport: pph.InstrumentRoundTripperInFlight(i.inflight,
 			pph.InstrumentRoundTripperCounter(i.requests,
 				pph.InstrumentRoundTripperTrace(trace,
-					pph.InstrumentRoundTripperDuration(i.duration, transport),
+					instrumentRoundTripperRequestSize(i.requestSize, instrumentRoundTripperResponseContentLength(i.responseContentLength,
+						pph.InstrumentRoundTripperDuration(i.duration, transport),
+					)),
 				),
 			),
 		),
 	}, reg.Register(i)
 }
 
+func instrumentRoundTripperRequestSize(obs prometheus.ObserverVec, next http.RoundTripper) pph.RoundTripperFunc {
+	return func(r *http.Request) (*http.Response, error) {
+		resp, err := next.RoundTrip(r)
+		if err == nil {
+			labels := prometheus.Labels{
+				"code":   fmt.Sprint(resp.StatusCode),
+				"method": strings.ToLower(r.Method),
+			}
+
+			obs.With(labels).Observe(float64(computeApproximateRequestSize(r)))
+		}
+		return resp, err
+	}
+}
+
+func computeApproximateRequestSize(r *http.Request) int {
+	s := 0
+	if r.URL != nil {
+		s += len(r.URL.String())
+	}
+
+	s += len(r.Method)
+	s += len(r.Proto)
+	for name, values := range r.Header {
+		s += len(name)
+		for _, value := range values {
+			s += len(value)
+		}
+	}
+	s += len(r.Host)
+
+	// N.B. r.Form and r.MultipartForm are assumed to be included in r.URL.
+
+	if r.ContentLength != -1 {
+		s += int(r.ContentLength)
+	}
+	return s
+}
+
+func instrumentRoundTripperResponseContentLength(obs prometheus.ObserverVec, next http.RoundTripper) pph.RoundTripperFunc {
+	return func(r *http.Request) (*http.Response, error) {
+		resp, err := next.RoundTrip(r)
+		if err == nil {
+			labels := prometheus.Labels{
+				"code":   fmt.Sprint(resp.StatusCode),
+				"method": strings.ToLower(r.Method),
+			}
+
+			obs.With(labels).Observe(float64(resp.ContentLength))
+		}
+		return resp, err
+	}
+}
+
 type outgoingInstrumentation struct {
-	duration    *prometheus.HistogramVec
-	requests    *prometheus.CounterVec
-	dnsDuration *prometheus.HistogramVec
-	tlsDuration *prometheus.HistogramVec
-	inflight    prometheus.Gauge
+	duration              *prometheus.HistogramVec
+	requests              *prometheus.CounterVec
+	dnsDuration           *prometheus.HistogramVec
+	tlsDuration           *prometheus.HistogramVec
+	inflight              prometheus.Gauge
+	requestSize           *prometheus.HistogramVec
+	responseContentLength *prometheus.HistogramVec
 }
 
 // Describe implements prometheus.Collector interface.
@@ -125,6 +207,8 @@ func (i *outgoingInstrumentation) Describe(in chan<- *prometheus.Desc) {
 	i.dnsDuration.Describe(in)
 	i.tlsDuration.Describe(in)
 	i.inflight.Describe(in)
+	i.requestSize.Describe(in)
+	i.responseContentLength.Describe(in)
 }
 
 // Collect implements prometheus.Collector interface.
